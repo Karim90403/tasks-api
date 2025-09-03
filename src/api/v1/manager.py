0b5abc3463e17a9588dd08ee8a823265f92691a8
karim <92757457+Karim90403@users.noreply.github.com>
2025-09-03 13:37:35 +0300

    Add endpoint to create project

--- src/api/v1/manager.py
+++ src/api/v1/manager.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends, status
 
 from core.dependencies import get_current_user
 from schemas.request.project_change import ChangeProjectRequest
+from schemas.request.project_create import ProjectCreate
 from schemas.user import UserInDB
 from services.manager_service import ManagerService, get_manager_service
 
@@ -16,6 +17,18 @@ async def get_projects(
     ):
     if current_user.role == "root":
         return await service.list_projects()
+
+@router.post(
+    "/projects",
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_project(
+    project: ProjectCreate,
+    service: ManagerService = Depends(get_manager_service),
+    current_user: UserInDB = Depends(get_current_user),
+):
+    if current_user.role == "root":
+        return await service.create_project(project)
 
 @router.patch(
     "/projects/{project_id}",
